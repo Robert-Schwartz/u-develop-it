cc4c8f52ea2a1c23d3216853a27f6cd2160635ca
Robert Schwartz <bschwartz23@gmail.com>
2021-05-28 15:29:12 -0700

    Build the Database Calls

--- server.js
+++ server.js
@@ -2,16 +2,37 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 // Express Middleware
 // ========================================
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'root',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
 // Routes
 // ========================================
 
-// Default response for any other request (Not Found)
+// database query to test the connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Catchall Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
